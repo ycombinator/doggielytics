67140561334f662385374f3d103519638816a36b
Shaunak Kashyap <ycombinator@gmail.com>
2015-03-15 19:05:16 -0700

    Refactoring main, entry point code from rest.

--- index.js
+++ index.js
@@ -1,54 +1,18 @@
 var tessel = require('tessel'),
-    accel = require('accel-mma84').use(tessel.port['A']);
+    accel = require('accel-mma84').use(tessel.port['A']),
+    Door = require('./lib/door.js');
 
-var app =  {
-  isDoorOpen: false,
-  isDoorSwinging: false,
-  isDogVisiting: false,
-  visits: [],
-
-  swingingTimeoutId: null,
-  currentVisit: null
-}
-
+var door = Door();
 
 // Initialize the accelerometer.
 accel.on('ready', function () {
-    // Stream accelerometer data
+  // Stream accelerometer data
   accel.on('data', function (xyz) {
     var x = xyz[0];
     if (x >= -0.9) {
-      if (!app.isDoorOpen) {
-        app.isDoorOpen = true;
-        clearTimeout(app.swingingTimeoutId);
-      }
+      door.open();
     } else {
-      if (app.isDoorOpen) {
-        app.isDoorOpen = false;
-        app.swingingTimeoutId = setTimeout(function() {
-          if (!app.isDogVisiting) {
-            // Dog just went out; start visit
-            app.isDogVisiting = true;
-            app.currentVisit = {
-              startTime: new Date()
-            };
-            
-            console.log("Dog went out for visit");
-            
-          } else {
-            // Dog just returned; end visit
-            app.isDogVisiting = false;
-            app.currentVisit.endTime = new Date();
-            
-            var visitSeconds = (app.currentVisit.endTime.getTime() - app.currentVisit.startTime.getTime()) / 1000;
-            console.log("Dog returned from visit; visit lasted " + visitSeconds + " seconds");
-            
-            app.visits.push(app.currentVisit);
-            app.currentVisit = null;
-          }
-          app.swingingTimeoutId = null;
-        }, 1500);
-      }
+      door.close();
     }
   });
 });
