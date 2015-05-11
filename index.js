a6906867a64f2b09dd0f7bccf75e5755dac5e493
Shaunak Kashyap <ycombinator@gmail.com>
2015-05-10 17:51:50 -0700

    Only start doing stuff when accel is ready.

--- index.js
+++ index.js
@@ -30,48 +30,49 @@ wifi.on('disconnect', function() {
 
 var door = new Door();
 
-accel.on('data', function (xyz) {
-  var x = xyz[0];
-  if (x >= -0.99) {
-    door.open();
-  } else {
-    door.close();
-  }
-});
-
-accel.on('error', function(err) {
-  console.log('Error:', err);
-});
-
 accel.on('ready', function () {
   console.log('Accelerometer ready...');
 
-  // Attempt wifi connection after 10 seconds; for some bizzare
-  // reason this delay is required when the board is powered
-  // standalone (as opposed to being connected to a computer)
-  setTimeout(function() {
-    wifi.connect({
-      ssid: wiFiSsid,
-      password: wiFiPassword
-    });
-  }, 7 * 1000);
-});
+  // // Attempt wifi connection after 10 seconds; for some bizzare
+  // // reason this delay is required when the board is powered
+  // // standalone (as opposed to being connected to a computer)
+  // setTimeout(function() {
+  //   wifi.connect({
+  //     ssid: wiFiSsid,
+  //     password: wiFiPassword
+  //   });
+  // }, 7 * 1000);
 
-door.on('visit-end', function(visit) {
-  console.log("Visit just ended: " + JSON.stringify(visit));
-  if (wifi.isConnected()) {
-    needle.post(
-      esBaseUrl + '/doggielytics/visits',
-      visit,
-      { json: true },
-      function(err, resp) {
-        if (err) {
-          return winston.error(err);
+  accel.on('data', function (xyz) {
+    var x = xyz[0];
+    if (x >= -0.99) {
+      door.open();
+    } else {
+      door.close();
+    }
+  });
+
+  accel.on('error', function(err) {
+    console.log('Error:', err);
+  });
+
+  door.on('visit-end', function(visit) {
+    console.log("Visit just ended: " + JSON.stringify(visit));
+    if (wifi.isConnected()) {
+      needle.post(
+        esBaseUrl + '/doggielytics/visits',
+        visit,
+        { json: true },
+        function(err, resp) {
+          if (err) {
+            return winston.error(err);
+          }
+          console.log("Indexed visit. ID = " + JSON.stringify(resp.body));
         }
-        console.log("Indexed visit. ID = " + JSON.stringify(resp.body));
-      }
-    );
-  } else {
-    console.warn('WiFi is not connected. Cannot index visit :(');
-  }
+      );
+    } else {
+      console.warn('WiFi is not connected. Cannot index visit :(');
+    }
+  });
+
 });
