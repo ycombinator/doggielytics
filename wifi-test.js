c66628ec7d004fe55b1004daa8cb44e8e74970a4
Shaunak Kashyap <ycombinator@gmail.com>
2019-11-01 13:18:36 -0700

    WIP

--- wifi-test.js
+++ wifi-test.js
@@ -1,10 +1,17 @@
 var tessel = require('tessel'),
-    wifi = require('wifi-cc3000'),
     needle = require('needle');
 
 var wiFiSsid = process.argv[2];
 var wiFiPassword = process.argv[3];
 var requestBinUrl = process.argv[4];
+
+console.log({ args: process.argv, wiFiSsid, wiFiPassword, requestBinUrl })
+
+const connectionSettings = {
+  ssid: wiFiSsid,
+  password: wiFiPassword,
+  security: 'wep'
+}
 
 var leds = {
   green: tessel.led[0],
@@ -28,39 +35,40 @@ blink(leds.blue, 5);
 
 // Try to connect to WiFi after 7 seconds
 setTimeout(function() {
-  wifi.connect({
-    ssid: wiFiSsid,
-    password: wiFiPassword
-  });
+  tessel.network.wifi.connect(connectionSettings);
 }, 7 * 1000);
 
 var numRequestsAttempted = 0;
 setInterval(function() {
     ++numRequestsAttempted;
     console.log("Attempting to send heartbeat message #" + numRequestsAttempted + "...");
-    if (wifi.isConnected()) {
-      needle.post(
-        requestBinUrl,
-        {
-            message: "WiFi is still connected!",
-            numRequestsAttempted: numRequestsAttempted
-                },
-        function(err, response) {
-          if (err) {
-            console.log("Error: " + err);
-          } else {
-            console.log("Response: " + response.body);
+    tessel.network.wifi.connection((err, settings) => {
+      console.log(err)
+      console.log(settings)
+      if (false) {
+        needle.post(
+          requestBinUrl,
+          {
+              message: "WiFi is still connected!",
+              numRequestsAttempted: numRequestsAttempted
+                  },
+          function(err, response) {
+            if (err) {
+              console.log("Error: " + err);
+            } else {
+              console.log("Response: " + response.body);
+            }
           }
-        }
-      );
-    } else {
-      console.log("WiFi is not connected right now. Cannot send heartbeat message :(");
-    }
+        );
+      } else {
+        console.log("WiFi is not connected right now. Cannot send heartbeat message :(");
+      }
+    })
 }, 20 * 1000);
   
 
 var numWiFiConnects = 0;
-wifi.on('connect', function(res) {
+tessel.network.wifi.on('connect', function(res) {
   ++numWiFiConnects;
   blink(leds.green, 3);
   console.log("WiFi is connected. IP address = " + res.ip);
@@ -80,18 +88,7 @@ wifi.on('connect', function(res) {
   );
 });
 
-wifi.on('disconnect', function() {
+tessel.network.wifi.on('disconnect', function() {
   console.log("WiFi is disconnected :( Retrying to connect...");
-  wifi.connect({
-    ssid: wiFiSsid,
-    password: wiFiPassword
-  });
-});
-
-wifi.on('timeout', function() {
-  console.log("WiFi has timed out :( Retrying to connect...");
-  wifi.connect({
-    ssid: wiFiSsid,
-    password: wiFiPassword
-  });
+  tessel.network.wifi.connect(connectionSettings);
 });
